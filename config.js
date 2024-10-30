108bf0c54cdc8a181ff795aa21476b610d3c1b62
Tharakadilshan0423 <tharakadilshan0313@gmail.com>
2024-10-30 19:13:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "ඉමේජ් යූආර් එල්",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://www.canva.com/design/DAGVDQWw34c/1GDMp7bSezahQvaMzG3gOA/watch?utm_content=DAGVDQWw34c&utm_campaign=designshare&utm_medium=link&utm_source=editor",
 ALIVE_MAG: process.env.ALIVE_MSG || "HELLOW, IAM NURO",
 };
