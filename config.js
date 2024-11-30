9413578fa510995048b9b5729ff1c166fca97bb3
LAKSIDU_NIMSARA <laksidunimsara200803041@gmail.com>
2024-12-01 00:00:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,27 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-///================================================MAIN-CONFIG=============================================
-SESSION_ID: process.env.SESSION_ID || "Put Session Id",  // ADD Your Session Id 
-MODE: process.env.MODE || "privet",  // Add Your Bot Mode 
-PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
-OWNER_REACT: process.env.OWNER_REACT || "true", //add true or false
-BOT_NAME: process.env.BOT_NAME || "NURO",
-///==========================================CUSTOM-SETTING-CONFIG=========================================
-AUTO_VOICE: process.env.AUTO_VOICE || "true", //add true or false
-AUTO_STICKER: process.env.AUTO_STICKER || "true", //add true or false
-AUTO_REPLY: process.env.AUTO_REPLY || "true", //add true or false
-ANTI_LINK: process.env.ANTI_LINK || "true", //add true or false
-ANTI_BAD: process.env.ANTI_BAD || "true", //add true or false
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true", //add true or false
-AUTO_REACT: process.env.AUTO_REACT || "true", //add true or false
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //add true or false
-///===============================================API-CONFIG==============================================
-///===============================================DONT-CHANGE-API=========================================
-GOOGLE_CX: process.env.GOOGLE_CX || "AIzaSyD93IeJsouK51zjKgyHAwBIAlqr-a8mnME", 
-PASTEBIN_API_KEY: process.env.PASTEBIN_API_KEY || "uh8QvO6vQJGtIug9WvjdTAPx_ZAFJAxn",
-OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
-///===============================================ALIVE-CONFIG=========================================
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/Md3nK0I.jpeg", //add alive img url
-ALIVE_MSG: process.env.ALIVE_MSG || "HI I AM WHATSAPP BOT☺️💞", //add alive msg
+SESSION_ID: process.env.SESSION_ID || "",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
 };
