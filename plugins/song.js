f3e260c9d178f880780f50be42bc60367e6ad973
Tharakadilshan0423 <tharakadilshan0313@gmail.com>
2024-12-06 07:12:28 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -7,7 +7,7 @@ const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
 const yts = require('yt-search')
 cmd({
-    pattern: "song",
+    pattern: "mp",
     desc: "To download songs.",
     react: "🎵",
     category: "download",
