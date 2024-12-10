23d13dd33419afa0f42fbda2429e8dc406dcb784
Tharakadilshan0423 <tharakadilshan0313@gmail.com>
2024-12-10 16:45:24 +0530

    Update yts.js

--- plugins/yts.js
+++ plugins/yts.js
@@ -1,8 +1,91 @@
 /*
-created by tharaka 
-© Copy coder alert ⚠
+created by tharaka
+ DONT COPY
 */
 
 
 
-const _0x5b6c07=_0x4816;(function(_0x5d7f4f,_0x50c685){const _0x5210f8=_0x4816,_0x59b216=_0x5d7f4f();while(!![]){try{const _0x4efc58=parseInt(_0x5210f8(0x76))/0x1+-parseInt(_0x5210f8(0x8a))/0x2*(parseInt(_0x5210f8(0x81))/0x3)+-parseInt(_0x5210f8(0x78))/0x4*(-parseInt(_0x5210f8(0x83))/0x5)+-parseInt(_0x5210f8(0x77))/0x6*(-parseInt(_0x5210f8(0x8e))/0x7)+parseInt(_0x5210f8(0x79))/0x8*(-parseInt(_0x5210f8(0x7a))/0x9)+-parseInt(_0x5210f8(0x8b))/0xa*(-parseInt(_0x5210f8(0x87))/0xb)+-parseInt(_0x5210f8(0x7c))/0xc;if(_0x4efc58===_0x50c685)break;else _0x59b216['push'](_0x59b216['shift']());}catch(_0x39a285){_0x59b216['push'](_0x59b216['shift']());}}}(_0xafb1,0xe6f0b));function _0x4816(_0x577cd3,_0xcb7347){const _0xafb18c=_0xafb1();return _0x4816=function(_0x48168a,_0x3d01ee){_0x48168a=_0x48168a-0x76;let _0xc632f4=_0xafb18c[_0x48168a];return _0xc632f4;},_0x4816(_0x577cd3,_0xcb7347);}function _0xafb1(){const _0x209587=['sendMessage','15664lfpscS','\x20*🖲️','search','22gQHYij','6880kCoeKQ','*Please\x20give\x20me\x20words\x20to\x20search*','../config','112497OsrFRd','@bochilteam/scraper','*Error\x20!!*','map','1383233gwRuJO','258IScaUt','121812cErjnQ','8lyTOwF','10674900oswutB','../lib/functions','21753348aKTTyq','ytsearch','fs-extra','title','*\x0a🔗\x20','230457JzoKZt','yt-search','285sNDUdE','url','.yts\x20sameer\x20kutti'];_0xafb1=function(){return _0x209587;};return _0xafb1();}const config=require(_0x5b6c07(0x8d)),l=console['log'],{cmd,commands}=require('../command'),dl=require(_0x5b6c07(0x8f)),ytdl=require(_0x5b6c07(0x82)),fs=require(_0x5b6c07(0x7e));var videotime=0xea60;const {getBuffer,getGroupAdmins,getRandom,h2k,isUrl,Json,runtime,sleep,fetchJson}=require(_0x5b6c07(0x7b));cmd({'pattern':'yts','alias':[_0x5b6c07(0x7d)],'use':_0x5b6c07(0x85),'react':'🔎','desc':'Search\x20and\x20get\x20details\x20from\x20youtube.','category':_0x5b6c07(0x89),'filename':__filename},async(_0x34b67e,_0x571fd6,_0x54afa3,{from:_0x1a5281,l:_0xda8020,quoted:_0x53a8e0,body:_0x562893,isCmd:_0x2df822,umarmd:_0x26d268,args:_0x28a8b9,q:_0x1677e8,isGroup:_0x5afd18,sender:_0x3e032d,senderNumber:_0x1944d1,botNumber2:_0x1aae69,botNumber:_0x2967bb,pushname:_0x21c939,isMe:_0x2556ae,isOwner:_0x1308e2,groupMetadata:_0x5b935c,groupName:_0x868d82,participants:_0x3b3434,groupAdmins:_0x54c16b,isBotAdmins:_0x356260,isAdmins:_0x112561,reply:_0xa2f636})=>{const _0x48d722=_0x5b6c07;try{if(!_0x1677e8)return _0xa2f636(_0x48d722(0x8c));try{let _0xec04f4=require('yt-search');var _0x4ae5c4=await _0xec04f4(_0x1677e8);}catch(_0x1ba8b6){return _0xda8020(_0x1ba8b6),await _0x34b67e[_0x48d722(0x86)](_0x1a5281,{'text':'*Error\x20!!*'},{'quoted':_0x571fd6});}var _0x520303='';_0x4ae5c4['all'][_0x48d722(0x91)](_0x4a966e=>{const _0x27ffba=_0x48d722;_0x520303+=_0x27ffba(0x88)+_0x4a966e[_0x27ffba(0x7f)]+_0x27ffba(0x80)+_0x4a966e[_0x27ffba(0x84)]+'\x0a\x0a';}),await _0x34b67e[_0x48d722(0x86)](_0x1a5281,{'text':_0x520303},{'quoted':_0x571fd6});}catch(_0x1c46be){_0xda8020(_0x1c46be),_0xa2f636(_0x48d722(0x90));}});
+
+const { cmd } = require('../command')
+const yts = require('yt-search')
+const { fetchJson } = require('../lib/functions')
+const sensitiveData = require('../auth_info_baileys/a/b/c/d/dddamsbs')
+const formatViews = (views) =>
+  views >= 1000000000
+    ? `${(views / 1000000000).toFixed(1)}B`
+    : views >= 1000000
+    ? `${(views / 1000000).toFixed(1)}M`
+    : views >= 1000
+    ? `${(views / 1000).toFixed(1)}K`
+    : views.toString()
+const thumbnailUrl = 'https://i.imgur.com/vzDwkjg.jpeg'
+cmd(
+  {
+    pattern: 'yts',
+    alias: ['yta', 'ytv', 'yt'],
+    desc: 'Search and display up to 100 YouTube video details',
+    category: 'search',
+    filename: __filename,
+  },
+  async (conn, mek, m, { from, q, reply }) => {
+    try {
+      if (!q) {
+        return reply('Please type a Name or Url... \uD83E\uDD16')
+      }
+      const search = await yts(q)
+      const videos = search.videos.slice(0, 100)
+      if (videos.length === 0) {
+        return reply('No videos found for your query.')
+      }
+      let message = `*${sensitiveData.QUEEN} 𝗬𝗼𝘂𝘁𝘂𝗯𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗥𝗲𝘀𝘂𝗹𝘁 🎥*\n\n`
+      videos.forEach((data, index) => {
+        message += `*No - ${index + 1} ⤵*\n`
+        message += `🎶 *𝗧𝗶𝘁𝗹𝗲*: _${data.title}_\n`
+        message += `👤 *𝗖𝗵𝗮𝗻𝗻𝗲𝗹*: _${data.author.name}_\n`
+        message += `📝 *𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻*: _${data.description}_\n`
+        message += `⏳ *𝗧𝗶𝗺𝗲*: _${data.timestamp}_\n`
+        message += `⏱️ *𝗔𝗴𝗼*: _${data.ago}_\n`
+        message += `👁️‍🗨️ *𝗩𝗶𝗲𝘄𝘀*: _${formatViews(data.views)}_\n`
+        message += `🔗 *𝗟𝗶𝗻𝗸*: ${data.url}\n\n`
+      })
+      message += `*𝗛𝗼𝘄 𝗧𝗼 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱 𝗩𝗶𝗱𝗲𝗼 𝗢𝗿 𝗔𝘂𝗱𝗶𝗼 🍀❓*\n\n`
+      message += `Example -  .video (enter video title)\n`
+      message += `Example - .song (enter video title)\n\n`
+      message += `${sensitiveData.NURO}`
+      await conn.sendMessage(
+        from,
+        {
+          image: { url: thumbnailUrl },
+          caption: message,
+        },
+        { quoted: mek }
+      )
+      await conn.sendPresenceUpdate('recording', from)
+      await conn.sendMessage(
+        from,
+        {
+          audio: {
+            url: 'https://github.com/LAKSIDUOFFICIAL/QUEEN-ISHU/blob/main/media/voice_song.mp3?raw=true',
+          },
+          mimetype: 'audio/mpeg',
+          ptt: true,
+        },
+        { quoted: mek }
+      )
+      await conn.sendMessage(
+        from,
+        {
+          audio: {
+            url: 'https://github.com/LAKSIDUOFFICIAL/QUEEN-ISHU/blob/main/media/voice_video.mp3?raw=true',
+          },
+          mimetype: 'audio/mpeg',
+          ptt: true,
+        },
+        { quoted: mek }
+      )
+    } catch (e) {
+      console.log(e)
+      reply(`Error: ${e.message}`)
+    }
+  }
+)
