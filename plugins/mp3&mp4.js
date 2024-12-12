1442fc9bfabbc61953b4d17c6c82b41264be04e3
Tharakadilshan0423 <tharakadilshan0313@gmail.com>
2024-12-12 16:59:18 +0530

    Update and rename music.js to mp3&mp4.js

--- plugins/mp3&mp4.js
+++ plugins/mp3&mp4.js
@@ -2,7 +2,7 @@ const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
 const yts = require('yt-search')
 cmd({
-    pattern: "song",
+    pattern: "mp3",
     desc: "To download songs.",
     react: "📼",
     category: "download",
